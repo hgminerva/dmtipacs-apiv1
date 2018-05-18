cc5f295582f3f21e79bab7640329cdbc7f4d469f
NoahOliverRIgonan <oliverrigonan@gmail.com>
2018-05-18 15:17:04 +0800

    Body Part and Modality APIs

---
+++ dmtipacs-api/ApiControllers/ApiMstBodyPartController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace dmtipacs_api.ApiControllers
+{
+    [Authorize]
+    [RoutePrefix("api/bodyPart")]
+    public class ApiMstBodyPartController : ApiController
+    {
+        // ============
+        // Data Context
+        // ============
+        private Data.dmtipacsdbDataContext db = new Data.dmtipacsdbDataContext();
+
+        // ================
+        // List - Body Part
+        // ================
+        [HttpGet, Route("list")]
+        public List<Entities.MstBodyPart> ListBodyPart()
+        {
+            var bodyParts = from d in db.MstBodyParts
+                            select new Entities.MstBodyPart
+                            {
+                                Id = d.Id,
+                                BodyPart = d.BodyPart
+                            };
+
+            return bodyParts.ToList();
+        }
+
+        // ===============
+        // Add - Body Part
+        // ===============
+        [Authorize, HttpPost, Route("add")]
+        public HttpResponseMessage AddBodyPart(Entities.MstBodyPart objBodyPart)
+        {
+            try
+            {
+                Data.MstBodyPart newBodyPart = new Data.MstBodyPart
+                {
+                    BodyPart = objBodyPart.BodyPart
+                };
+
+                db.MstBodyParts.InsertOnSubmit(newBodyPart);
+                db.SubmitChanges();
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // ==================
+        // Update - Body Part
+        // ==================
+        [Authorize, HttpPut, Route("update/{id}")]
+        public HttpResponseMessage UpdateBodyPart(String id, Entities.MstBodyPart objBodyPart)
+        {
+            try
+            {
+                var bodyPart = from d in db.MstBodyParts
+                               where d.Id == Convert.ToInt32(id)
+                               select d;
+
+                if (bodyPart.Any())
+                {
+                    var updateBodyPart = bodyPart.FirstOrDefault();
+                    updateBodyPart.BodyPart = objBodyPart.BodyPart;
+
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // ==================
+        // Delete - Body Part
+        // ==================
+        [Authorize, HttpDelete, Route("delete/{id}")]
+        public HttpResponseMessage DeleteBodyPart(String id)
+        {
+            try
+            {
+                var bodyPart = from d in db.MstBodyParts
+                               where d.Id == Convert.ToInt32(id)
+                               select d;
+
+                if (bodyPart.Any())
+                {
+                    db.MstBodyParts.DeleteOnSubmit(bodyPart.First());
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
---
+++ dmtipacs-api/ApiControllers/ApiMstModalityController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace dmtipacs_api.ApiControllers
+{
+    [Authorize]
+    [RoutePrefix("api/modality")]
+    public class ApiMstModalityController : ApiController
+    {
+        // ============
+        // Data Context
+        // ============
+        private Data.dmtipacsdbDataContext db = new Data.dmtipacsdbDataContext();
+
+        // ===============
+        // List - Modality
+        // ===============
+        [HttpGet, Route("list")]
+        public List<Entities.MstModality> ListModality()
+        {
+            var modalities = from d in db.MstModalities
+                             select new Entities.MstModality
+                             {
+                                 Id = d.Id,
+                                 Modality = d.Modality
+                             };
+
+            return modalities.ToList();
+        }
+
+        // ==============
+        // Add - Modality
+        // ==============
+        [Authorize, HttpPost, Route("add")]
+        public HttpResponseMessage AddModality(Entities.MstModality objModality)
+        {
+            try
+            {
+                Data.MstModality newModality = new Data.MstModality
+                {
+                    Modality = objModality.Modality
+                };
+
+                db.MstModalities.InsertOnSubmit(newModality);
+                db.SubmitChanges();
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // =================
+        // Update - Modality
+        // =================
+        [Authorize, HttpPut, Route("update/{id}")]
+        public HttpResponseMessage UpdateModality(String id, Entities.MstModality objModality)
+        {
+            try
+            {
+                var modality = from d in db.MstModalities
+                               where d.Id == Convert.ToInt32(id)
+                               select d;
+
+                if (modality.Any())
+                {
+                    var updateModality = modality.FirstOrDefault();
+                    updateModality.Modality = objModality.Modality;
+
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // =================
+        // Delete - Modality
+        // =================
+        [Authorize, HttpDelete, Route("delete/{id}")]
+        public HttpResponseMessage DeleteModality(String id)
+        {
+            try
+            {
+                var modality = from d in db.MstModalities
+                               where d.Id == Convert.ToInt32(id)
+                               select d;
+
+                if (modality.Any())
+                {
+                    db.MstModalities.DeleteOnSubmit(modality.First());
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
